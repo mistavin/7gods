c1692308dc799925fbfaeb34f9a99b528a830ac0
mistavin <vinblaze090@gmail.com>
2025-02-19 21:41:27 +0100

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,147 +1,168 @@
-(function () {
-  "use strict";
+(function ($) {
+    'use strict';
 
-  /**
-   * Easy selector helper function
-   */
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
-  }
+    var $window = $(window);
 
-  /**
-   * Easy event listener function
-   */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
-      }
-    }
-  }
-
-  /**
-   * Easy on scroll event listener 
-   */
-  const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
-
-  /**
-   * Navbar links active state on scroll
-   */
-  let navbarlinks = select('#navbar .scrollto', true)
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
-      if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
-      } else {
-        navbarlink.classList.remove('active')
-      }
-    })
-  }
-  window.addEventListener('load', navbarlinksActive)
-  onscroll(document, navbarlinksActive)
-
-  /**
-   * Scrolls to an element with header offset
-   */
-  const scrollto = (el) => {
-    let header = select('#header')
-    let offset = header.offsetHeight
-
-    let headerNav = select('#nav-container')
-    let offsetNav = headerNav.offsetHeight
-
-
-    if (!header.classList.contains('header-scrolled')) {
-      offset -= 20
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
     }
 
-    if (!headerNav.classList.contains('nav-container-scrolled')) {
-        offsetNav -= 20
+    /**
+     * Easy event listener function
+     */
+    const on = (type, el, listener, all = false) => {
+        let selectEl = select(el, all)
+        if (selectEl) {
+            if (all) {
+                selectEl.forEach(e => e.addEventListener(type, listener))
+            } else {
+                selectEl.addEventListener(type, listener)
+            }
+        }
     }
 
-    let elementPos = select(el).offsetTop
-    window.scrollTo({
-      top: elementPos - offset,
-      behavior: 'smooth'
-    })
-  }
+    /**
+     * Easy on scroll event listener 
+     */
+    const onscroll = (el, listener) => {
+        el.addEventListener('scroll', listener)
+    }
 
-  /**
-   * Toggle .header-scrolled class to #header when page is scrolled
+    /**
+     * Navbar links active state on scroll
+     */
+    let navbarlinks = select('#navbar .scrollto', true)
+    const navbarlinksActive = () => {
+        let position = window.scrollY + 200
+        navbarlinks.forEach(navbarlink => {
+            if (!navbarlink.hash) return
+            let section = select(navbarlink.hash)
+            if (!section) return
+            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+                navbarlink.classList.add('active')
+            } else {
+                navbarlink.classList.remove('active')
+            }
+        })
+    }
+    window.addEventListener('load', navbarlinksActive)
+    onscroll(document, navbarlinksActive)
+
+    /**
+     * Scrolls to an element with header offset
+     */
+    const scrollto = (el) => {
+        let header = select('#header')
+        let offset = header.offsetHeight
+
+        let headerNav = select('#nav-container')
+        let offsetNav = headerNav.offsetHeight
+
+
+        if (!header.classList.contains('header-scrolled')) {
+            offset -= 20
+        }
+
+        if (!headerNav.classList.contains('nav-container-scrolled')) {
+            offsetNav -= 20
+        }
+
+        let elementPos = select(el).offsetTop
+        window.scrollTo({
+            top: elementPos - offset,
+            behavior: 'smooth'
+        })
+    }
+
+    /**
+     * Toggle .header-scrolled class to #header when page is scrolled
+     */
+    let selectHeader = select('#header')
+    if (selectHeader) {
+        const headerScrolled = () => {
+            if (window.scrollY > 100) {
+                selectHeader.classList.add('header-scrolled')
+            } else {
+                selectHeader.classList.remove('header-scrolled')
+            }
+        }
+        window.addEventListener('load', headerScrolled)
+        onscroll(document, headerScrolled)
+    }
+
+
+    let selectMobileHeader = select('#nav-container')
+    if (selectMobileHeader) {
+        const headerMobileScrolled = () => {
+            if (window.scrollY > 100) {
+                selectMobileHeader.classList.add('nav-container-scrolled')
+            } else {
+                selectMobileHeader.classList.remove('nav-container-scrolled')
+            }
+        }
+        window.addEventListener('load', headerMobileScrolled)
+        onscroll(document, headerMobileScrolled)
+    }
+
+
+    /**
+   * Animation on scroll function and init
    */
-  let selectHeader = select('#header')
-  if (selectHeader) {
-    const headerScrolled = () => {
-      if (window.scrollY > 100) {
-        selectHeader.classList.add('header-scrolled')
-      } else {
-        selectHeader.classList.remove('header-scrolled')
-      }
+    function aosInit() {
+        AOS.init({
+            duration: 1000,
+            easing: 'ease-in-out',
+            once: false,
+            mirror: false
+        });
     }
-    window.addEventListener('load', headerScrolled)
-    onscroll(document, headerScrolled)
-  }
-  
+    window.addEventListener('load', aosInit);
 
-  let selectMobileHeader = select('#nav-container')
-  if (selectMobileHeader) {
-    const headerMobileScrolled = () => {
-      if (window.scrollY > 100) {
-        selectMobileHeader.classList.add('nav-container-scrolled')
-      } else {
-        selectMobileHeader.classList.remove('nav-container-scrolled')
-      }
-    }
-    window.addEventListener('load', headerMobileScrolled)
-    onscroll(document, headerMobileScrolled)
-  }
-
-
-  /**
- * Animation on scroll function and init
- */
-  function aosInit() {
-    AOS.init({
-      duration: 1000,
-      easing: 'ease-in-out',
-      once: false,
-      mirror: false
+    /**
+     * Scroll with ofset on page load with hash links in the url
+     */
+    window.addEventListener('load', () => {
+        if (window.location.hash) {
+            if (select(window.location.hash)) {
+                scrollto(window.location.hash)
+            }
+        }
     });
-  }
-  window.addEventListener('load', aosInit);
 
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
-  window.addEventListener('load', () => {
-    if (window.location.hash) {
-      if (select(window.location.hash)) {
-        scrollto(window.location.hash)
-      }
-    }
-  });
+    /*------------------
+         Preloader
+     --------------------*/
+    //   $(window).on('load', function () {
+    //     $(".loader").fadeOut();
+    //     $("#preloder").delay(200).fadeOut("slow");
 
-  /*------------------
-       Preloader
-   --------------------*/
-  $(window).on('load', function () {
-    $(".loader").fadeOut();
-    $("#preloder").delay(200).fadeOut("slow");
+    //   });
+    // Preloader Active Code
+    $window.on('load', function () {
+        $('#preloader').fadeOut('slow', function () {
+            $(this).remove();
+        });
+    });
 
-  });
+    var $listCollection = $(".questions-area > ul > li");
+    var $firstItem = $listCollection.first();
+    $listCollection.first().addClass("question-show");
+    setInterval(function () {
+        var $activeItem = $(".question-show")
+        $activeItem.removeClass("question-show");
+        var $nextItem = $activeItem.closest('li').next();
+        if ($nextItem.length == 0) {
+            $nextItem = $firstItem;
+        }
+        $nextItem.addClass("question-show");
+    }, 5000);
 
-})()
+})(jQuery);
