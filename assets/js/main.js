31a5b3b3c2556b64c91ee55cb1e1ce34747ae52a
mistavin <vinblaze090@gmail.com>
2025-02-20 05:36:38 +0100

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -141,28 +141,13 @@
          Preloader
      --------------------*/
     //   $(window).on('load', function () {
-    //     $(".loader").fadeOut();
-    //     $("#preloder").delay(200).fadeOut("slow");
-
+    //     // $(".loader").fadeOut();
+    //     $("#preloader-active").delay(200).fadeOut("slow");
     //   });
-    // Preloader Active Code
-    $window.on('load', function () {
-        $('#preloader').fadeOut('slow', function () {
-            $(this).remove();
-        });
-    });
-
-    var $listCollection = $(".questions-area > ul > li");
-    var $firstItem = $listCollection.first();
-    $listCollection.first().addClass("question-show");
-    setInterval(function () {
-        var $activeItem = $(".question-show")
-        $activeItem.removeClass("question-show");
-        var $nextItem = $activeItem.closest('li').next();
-        if ($nextItem.length == 0) {
-            $nextItem = $firstItem;
-        }
-        $nextItem.addClass("question-show");
-    }, 5000);
+     // Page loading
+    //  $(window).on("load", function () {
+    //     $("#preloader-active").delay(450).fadeOut("slow");
+        
+    // });
 
 })(jQuery);
